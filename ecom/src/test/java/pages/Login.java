4f03cea1c52a30fba81d66f3118a09a49fab8867
Sanjay Kamade <sanjaykamade@yahoo.com>
2022-12-28 01:20:43 +0530

    changes

---
+++ ecom/src/test/java/pages/Login.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login {
+public static void main(String[] args) {
+	WebDriver driver;
+	By login = By.name("Login");
+	By user = By.className("_2IX_2- VJZDxU");
+	
+//	public Login(WebDriver driver) {
+//		this.driver=driver;
+//	}
+//	
+	
+}
+}
